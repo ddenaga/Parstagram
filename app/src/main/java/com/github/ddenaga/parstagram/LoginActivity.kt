23c179f6c5be5807d9a9b9338c63a489a15c42a2
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-03-27 22:17:21 -0700

    Implemented Parse sign up and log in

--- app/src/main/java/com/github/ddenaga/parstagram/LoginActivity.kt
+++ app/src/main/java/com/github/ddenaga/parstagram/LoginActivity.kt
@@ -15,12 +15,49 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Switch to the MainActivity if there's a user logged in
+        if (ParseUser.getCurrentUser() != null) {
+            // goToMainActivity()
+        }
+
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         btnLogin.setOnClickListener {
             val username = findViewById<EditText>(R.id.etUsername).text.toString()
             val password = findViewById<EditText>(R.id.etPassword).text.toString()
 
             loginUser(username, password)
+        }
+
+        val btnSignup = findViewById<Button>(R.id.btnSignup)
+        btnSignup.setOnClickListener {
+            val username = findViewById<EditText>(R.id.etUsername).text.toString()
+            val password = findViewById<EditText>(R.id.etPassword).text.toString()
+
+            signupUser(username, password)
+        }
+    }
+
+    private fun signupUser(username: String, password: String) {
+        // Create a new ParseUser
+        val user = ParseUser()
+
+        // Set the fields for the user
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // Sign up succeeded.
+                Log.i(TAG, "Signup was successful.")
+                Toast.makeText(this, "Signup was a success!", Toast.LENGTH_SHORT).show()
+                goToMainActivity()
+            }
+            else {
+                // Sign up failed.
+                Log.e(TAG, "Signup failed: $e")
+                Toast.makeText(this, "Signup failed.", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
         }
     }
 
